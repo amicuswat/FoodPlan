5f637649b811a4cc051099596c799ea534a68683
amicuswat <amicuswat@gmail.com>
2022-09-23 16:57:31 +0300

    Доработал админку

--- foodmanager/admin.py
+++ foodmanager/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
-from .models import User, Dish, DishStep, Product, DishProduct, UserDish, Tag, UsedTag
+from .models import User, Dish, DishStep, Product, DishProduct, UserDish, Tag, \
+    UsedTag
+
+class DishStepInline(admin.TabularInline):
+    model = DishStep
+
+class DishProductsInline(admin.TabularInline):
+    model = DishProduct
+
+class UsedTagInline(admin.TabularInline):
+    model = UsedTag
+
+class DishAdmin(admin.ModelAdmin):
+    inlines = [
+        UsedTagInline,
+        DishStepInline,
+        DishProductsInline,
+    ]
 
 admin.site.register(User)
-admin.site.register(Dish)
+admin.site.register(Dish, DishAdmin)
 admin.site.register(DishStep)
 admin.site.register(Product)
 admin.site.register(DishProduct)
